c45366a8bddfdd010c1aa1646e9f824cff35baed
normak <km123456789@gmail.com>
2018-06-30 16:46:45 -0500

    Updated for Python 3 and added wrapping

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,15 +1,23 @@
-#A basic implementation of caesar's cipher
+def cipher (content, shift):
+  encrypted = ""
+  for char in content:
+    if (ord(char) >= 65 and ord(char) <= 90):
+      if ((ord(char) + shift) > 90):
+        encrypted += chr(64 + ((ord(char) + shift) % 90))
+      else:
+        encrypted += chr(ord(char) + shift)
+    elif (ord(char) >= 97 and ord(char) <= 122):
+      if ((ord(char) + shift) > 122):
+        encrypted += chr(96 + ((ord(char) + shift) % 122))
+      else:
+        encrypted += chr(ord(char) + shift)
+    else:
+      encrypted += char
 
-def cipher(content, shift):
-	encrypted = ''
-	for char in content:
-		if (char != ' '):
-			encrypted += str(unichr(ord(char) + shift))
-		else:
-			encrypted += ' '
-	return encrypted
+  return encrypted
 
-content = str(raw_input("Enter a phrase that you'd like to encrypt: "))
-shift = int(raw_input("Enter the shift you'd like to use: "))
 
-print cipher(content, shift)
+content = str(input("Enter a phrase that you'd like to encrypt: "))
+shift = int(input("Enter the shift you'd like to use: "))
+
+print(cipher(content,shift))
